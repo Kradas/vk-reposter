0965e0daa68c6ce061de84825a30480be1750343
kradas <kraevds@gmail.com>
2015-10-19 23:15:14 +0300

    config update

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.exports = {
+    vk: {
+        appId: 5103160,
+        appSecret: 'YOUR_APP_SECRET',
+        token: 'YOUR_ACCESS_TOKEN',
+        targetGroupId: -47741226,
+        pullGroupIds: [-28261334, -33393308]
+    }
+};
