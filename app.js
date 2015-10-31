0ad5ea42183df497e6e8043590c6abd6df8d0c8e
kradas <kraevds@gmail.com>
2015-10-31 18:55:50 +0300

    Adding a database

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ var config = require('config');
 var async = require('async');
 var request = require('request');
 var qs = require('qs');
+var firebase = require('firebase');
 
-function compareLikes(postOne, postTwo) {
-    return postTwo.likes.count - postOne.likes.count;
-}
+var database = new firebase(config.vk.firebaseLink);
 
 function getAttachmentsStr(attachments) {
     var attachmentsStr = '';
+    var likna = ''
+    var flag = 0;
+    if ('link' in attachments) {
+        linka = linka + attachments[attachments.type].url;
+    }
     attachments.forEach(function(item, i) {
-        if (attachmentsStr.length > 0){
+        if (attachmentsStr.length > 0) {
             attachmentsStr = attachmentsStr + ',';
         }
+        
         if (item.type == 'link') {
             attachmentsStr = attachmentsStr + item[item.type].url;
         } else {
@@ -35,8 +40,37 @@ function requestAPI(reqMethod, apiMethod, data, callback) {
     });
 }
 
-// Get the last added post time
-requestAPI('GET', 'wall.get', { 'owner_id' : config.vk.targetGroupId, 'offset' : 0, 'count' : 1}, function(err, post) {
+function postsToRepost(postsToAdd) {
+
+    //sorting by likes
+    postsToAdd.sort(function(postOne, postTwo) {
+        return postTwo.likes.count - postOne.likes.count;
+    });
+
+    //define total amount of likes
+    var totalLikes = postsToAdd.reduce(function(last, next) {
+        return last += next.likes.count;
+    }, 0);
+
+    //proportion for each post in total likes
+    var postsLikesProportion = postsToAdd.map(function(post, i) {
+        return { id: i + 1, likesProportion: 100 * post.likes.count / totalLikes, count: post.likes.count };
+    });
+
+    for (var i = postsLikesProportion.length - 2; postsLikesProportion[i]; i--) {
+        postsLikesProportion[i].likesProportion += postsLikesProportion[i + 1].likesProportion;
+    }
+
+    var numberOfPosts = postsLikesProportion.filter(function(percent) {
+        return percent.likesProportion > 70;
+    });
+
+    return numberOfPosts.length > 10 ? 10 : numberOfPosts.length;
+}
+
+
+// Get the last added post time 
+requestAPI('GET', 'wall.get', { owner_id : config.vk.targetGroupId, offset : 0, count : 1}, function(err, post) {
 
     if (err) {
         console.log('GET ERROR:', err);
@@ -51,71 +85,80 @@ requestAPI('GET', 'wall.get', { 'owner_id' : config.vk.targetGroupId, 'offset' :
     console.log(lastPostAddTime);
 
     async.eachSeries(config.vk.pullGroupIds, function(groupId, nextGroup) {
+
         //get set of posts from targeted group that were added after last post from my group (up to 50)
-        requestAPI('GET', 'wall.get', {'owner_id' : groupId, 'offset' : 1, 'count' : 50}, function(err, posts) {
+        requestAPI('GET', 'wall.get', {owner_id : groupId, offset : 1, count : 50}, function(err, posts) {
 
             if (err) {
                 return nextGroup(err);
             }
 
-            var postsArr = posts.response.items;
-            var currPostTime = 0;
+            database.child(groupId.toString()).once('value', function (snapshot) {
 
-            var postsToAdd = postsArr.filter(function(post) {
-                return post.date > lastPostAddTime;
-            });
+                var lastAddedPostTime = 0;
 
-            if (!postsToAdd.length) {
-                console.log('nothing to add');
-                return;
-            }
+                if (!snapshot.val()) {
+                    database.child(groupId).set({lastAddedPostTime : 0});
+                } else {
+                    lastAddedPostTime = snapshot.val().lastAddedPostTime;
+                }
 
-            console.log('last post was added at: ' + currPostTime + ', amount of post is: ' + postsToAdd.length);
-
-            postsToAdd.sort(function(postOne, postTwo) {
-                return postTwo.likes.count - postOne.likes.count;
-            });
-
-
-            var totalLikes = postsToAdd.reduce(function(last, next) {
-                return last += next.likes.count;
-            }, 0);
-
-            console.log('TOTAL LIKES:', totalLikes);
-
-            var postsLikesProportion = postsToAdd.map(function(post, i) {
-                return { id: i, likesProportion: 100 * post.likes.count / totalLikes, count: post.likes.count };
-            });
-
-            async.eachSeries(postsToAdd.slice(0, 5), function(post, next) {
-
-                var attachmentsStr = getAttachmentsStr(post.attachments);
-
-                console.log('POST ID:', post.id);
-
-                requestAPI('POST', 'wall.post', {
-                    owner_id: config.vk.targetGroupId,
-                    from_group: 1,
-                    message: postsArr[0].text,
-                    attachments: attachmentsStr
-                }, function(err) {
-
-                    if (err) {
-                        console.log('POST ERROR:', err);
-                    }
-
-                    setTimeout(next, 1000);
+                var postsToAdd = posts.response.items.filter(function(post) {
+                    return post.date > lastAddedPostTime;
                 });
 
-            }, function(err) {
-                if (err) {
-                    console.log('POST ERROR:', err);
-                    nextGroup(err);
+                database.child(groupId).update({lastAddedPostTime : posts.response.items[0].date});
+
+                if (!postsToAdd.length) {
+                    console.log('nothing to add');
+                    nextGroup();
                     return;
                 }
 
-                console.log('GROUP DONE!!!');
-                nextGroup();
+                console.log('The last post was added at: ' + lastAddedPostTime + ', amount of post is: ' + postsToAdd.length);
+
+                var numberOfPostsToPost = postsToRepost(postsToAdd);
+
+                console.log(numberOfPostsToPost + ' posts will be added');
+
+                async.eachSeries(postsToAdd.slice(0, numberOfPostsToPost), function(post, next) {
+
+                    var attachmentsStr;
+
+                    if (post.attachments) {
+                        var attachmentsStr = getAttachmentsStr(post.attachments);
+                    } else {
+                        var attachmentsStr = '';
+                    }
+
+                    console.log('POST ID:', post.id);
+
+                    requestAPI('POST', 'wall.post', {
+                        owner_id: config.vk.targetGroupId,
+                        from_group: 1,
+                        message: post.text,
+                        attachments: attachmentsStr,
+                    }, function(err) {
+                        if (err) {
+                            console.log('POST ERROR:', err);
+                        }
+
+                        setTimeout(next, 1000);
+                    });
+
+                }, function(err) {
+                    if (err) {
+                        console.log('POST ERROR:', err);
+                        nextGroup(err);
+                        return;
+                    }
+
+                    console.log('GROUP DONE!!!');
+                    nextGroup();
+                });
+            }, function (err) {
+                console.log(err);
+                process.exit();
             });
         });
     }, function(err) {
@@ -125,5 +168,6 @@ requestAPI('GET', 'wall.get', { 'owner_id' : config.vk.targetGroupId, 'offset' :
         }
 
         console.log('GROUPS DONE!!!');
+        process.exit();
     }); 
 });
